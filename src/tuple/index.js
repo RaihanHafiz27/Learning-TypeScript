3aa51a4b21b5fc47d6505d144fe668848bcb1049
muhammad raihan <muhammadraihan27072@gmail.com>
2025-10-03 20:09:20 +0700

    array tuple

---
+++ src/tuple/index.js
@@ -0,0 +1,23 @@
+var person;
+person = ["John Doe", 27];
+console.log(person);
+// the sequence must be correct
+// person = [27, "John Doe"]
+// console.log(person);
+// tuple with more than 2 elements
+var product;
+product = [1, "Xiaomi", true];
+console.log(product);
+//  tuple in function return
+var getUser = function () {
+    return [27, "Hello World"];
+};
+console.log(getUser());
+// optional tuple element
+var point;
+point = [100, 200, 300];
+console.log(point);
+point = [100, 200];
+console.log(point);
+point = [100];
+console.log();
